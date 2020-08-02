cf73e5534f291eaa1087303d3faa06a2728d3ff4
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-08-02 12:00:34 +0900

    [add]variable practice

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -1,11 +1,25 @@
+//真偽値
+print("北村" == "北村")
+print("あ" == "い")
+
 //変数
+var f:String
+f = "orange"
+print(f)
+
+var n:Int
+n = 5
+print(n)
+
+//変数は省略もできる
 var fruit = "apple"
 print(fruit)
+
+var number = 2
+print(number)
 
 //定数
 let vegetable = "callot"
 print(vegetable)
 
-//真偽値
-print("北村" == "北村")
-print("あ" == "い")
+
