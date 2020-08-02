37bc409561575fd16d9641771f9a074608b84d7c
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-08-02 15:37:07 +0900

    [add]variable count

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -24,6 +24,14 @@ print(vegetable)
 
 //配列
 var arr = [1,2,3]
-print(arr[1]) 
+print(arr[1])
+
+//配列は基本的に同じデータ型した入れれないが、Any型を使えば可能
+var arr1:[Any] = [1,3.14,"あ"]
+print(arr1[2])
+
+//カウント
+var member = ["aさん","bさん","cさん"]
+print(member.count)
 
 
